9d446e749a3b32fef5da4133d04112793c285ec3
mzc <mzc@live.com>
2026-03-22 22:04:49 +0800

    添加 Hysteria 和 V2Ray 配置模板，更新配置生成脚本以支持新模板

--- conf_gen.sh
+++ conf_gen.sh
@@ -3,8 +3,8 @@ apk add --no-cache jq > /dev/null 2>&1
 
 # v2ray config
 USER_DATA=$(jq -c '.' /users.json)
-sed -e 's|"##_USERS_##"|'"$USER_DATA"'|g' /v2ray-server.json > /conf/v2ray-server.json
+sed -e 's|"##_USERS_##"|'"$USER_DATA"'|g' /v2ray.tmpl.json > /conf/v2ray.json
 
 # hysteria config: replace userpass placeholder with users from users.json
 HY_USERS=$(jq -r '[.[] | "    " + .email + ": " + .id] | join("\n")' /users.json)
-awk -v users="$HY_USERS" '{gsub(/    ##_HYSTERIA_USERS_##/, users); print}' /hysteria.yml > /conf/hysteria.yml
+awk -v users="$HY_USERS" '{gsub(/    ##_HYSTERIA_USERS_##/, users); print}' /hysteria.tmpl.yml > /conf/hysteria.yml
